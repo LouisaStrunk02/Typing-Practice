fa357d39243d3d5a06705d1051fae45d62cbcd15
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-09-15 14:23:59 +0200

    generate text on load

--- typing.js
+++ typing.js
@@ -1,7 +1,38 @@
-file = "https://random-word-api.herokuapp.com/word?number=" + 3;
-
-async function getRandomWords() {
+async function getFirstText() {
+  file = "https://random-word-api.herokuapp.com/word?number=" + 10;
   words = await fetch(file);
   data = await words.json();
-  console.log(data);
+  text = data.join(" ");
+  document.querySelector(".main__textarea").innerHTML = text;
+}
+
+function hitEnter(event) {
+  if (event.key === "Enter") {
+    getRandomWords();
+  }
+  else {
+    return;
+  }
+}
+
+async function getRandomWords() {
+  const MAXWORDS = 30;
+  textLength = document.getElementById("textLength").value;
+
+  if (textLength > MAXWORDS) {
+    document.querySelector(".input__error").innerHTML = "Max words: 30!";
+    document.querySelector(".main__textarea").innerHTML = "";
+  }
+  else if (textLength <= 0) {
+    document.querySelector(".input__error").innerHTML = "That`s impossible!";
+    document.querySelector(".main__textarea").innerHTML = "";
+  }
+  else {
+    file = "https://random-word-api.herokuapp.com/word?number=" + textLength;
+    words = await fetch(file);
+    data = await words.json();
+    text = data.join(" ");
+    document.querySelector(".main__textarea").innerHTML = text;
+    document.querySelector(".input__error").innerHTML = "";
+  }
 }
