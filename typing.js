8106758ab68912745516c5f486a11dd24f170a31
Louisa Strunk <louisa@MBP-von-Louisa.office.5minds.de>
2022-09-21 09:51:51 +0200

    add checkKey function

--- typing.js
+++ typing.js
@@ -1,9 +1,25 @@
-async function getFirstText() {
-  file = "https://random-word-api.herokuapp.com/word?number=" + 10;
-  words = await fetch(file);
-  data = await words.json();
-  text = data.join(" ");
-  document.querySelector(".main__textarea").innerHTML = text;
+body = document.getElementsByTagName("body")[0];
+restText = document.querySelector(".main__textarea--rest");
+errorMessage = document.querySelector(".input__error");
+incorrectKey = false;
+textLength = 10;
+
+async function getRandomText() {
+  file = "https://random-word-api.herokuapp.com/word?number=" + textLength;
+  response = await fetch(file);
+  words = await response.json();
+  text = words.join(" ");
+  restText.innerHTML = text;
+  current = document.createElement("SPAN");
+  current.className = "main__textarea--current";
+  current.innerHTML = restText.textContent[0];
+  restText.textContent = restText.textContent.slice(1);
+  textarea = document.getElementsByClassName("main__textarea");
+  textarea[0].insertBefore(current, restText);
+  errorMessage.innerHTML = "";
+  incorrectKey = false;
+  document.getElementById("newText").blur();
+  document.getElementById("input").blur();
 }
 
 function hitEnter(event) {
@@ -17,22 +33,56 @@ function hitEnter(event) {
 
 async function getRandomWords() {
   const MAXWORDS = 30;
-  textLength = document.getElementById("textLength").value;
+  textLength = document.getElementById("input").value;
 
   if (textLength > MAXWORDS) {
-    document.querySelector(".input__error").innerHTML = "Max words: 30!";
-    document.querySelector(".main__textarea").innerHTML = "";
+    errorMessage.innerHTML = "Max words: 30!";
+    restText.innerHTML = "";
   }
   else if (textLength <= 0) {
-    document.querySelector(".input__error").innerHTML = "That`s impossible!";
-    document.querySelector(".main__textarea").innerHTML = "";
+    errorMessage.innerHTML = "That`s impossible!";
+    restText.innerHTML = "";
   }
   else {
-    file = "https://random-word-api.herokuapp.com/word?number=" + textLength;
-    words = await fetch(file);
-    data = await words.json();
-    text = data.join(" ");
-    document.querySelector(".main__textarea").innerHTML = text;
-    document.querySelector(".input__error").innerHTML = "";
+    const corrects = document.querySelectorAll('.main__textarea--correct');
+    const incorrects = document.querySelectorAll('.main__textarea--incorrect');
+
+    current.remove();
+
+    corrects.forEach(correctKey => {
+      correctKey.remove();
+    });
+
+    incorrects.forEach(incorrectKey => {
+      incorrectKey.remove();
+    });
+
+    getRandomText();
   }
 }
+
+body.addEventListener("keydown", function checkKey(event) {
+
+  if (event.key == current.textContent[0] && !incorrectKey) {
+    current.className = "main__textarea--correct";
+    current = document.createElement("SPAN");
+    current.className = "main__textarea--current";
+    current.textContent = restText.textContent[0];
+    restText.textContent = restText.textContent.slice(1);
+    textarea = document.getElementsByClassName("main__textarea");
+    textarea[0].insertBefore(current, restText);
+  }
+  else if (event.key != current.textContent[0] && !incorrectKey) {
+    incorrectKey = true;
+  }
+  else if (event.key == current.textContent[0] && incorrectKey) {
+    current.className = "main__textarea--incorrect";
+    current = document.createElement("SPAN");
+    current.className = "main__textarea--current";
+    current.textContent = restText.textContent[0];
+    restText.textContent = restText.textContent.slice(1);
+    textarea = document.getElementsByClassName("main__textarea");
+    textarea[0].insertBefore(current, restText);
+    incorrectKey = false;
+  }
+})
